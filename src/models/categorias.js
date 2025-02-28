12938d2008e367183ba12b5145304abaddfa28b2
Bruno <fubequinhabruno@gmail.com>
2025-02-28 14:30:30 -0300

    Adicionado o ProjetoCrud ao Github

---
+++ src/models/categorias.js
@@ -0,0 +1,28 @@
+class categorias {
+    constructor(id, nome, descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    };
+
+
+    setNome(nome) {
+        this._nome = nome 
+    };
+
+    setDescricao(descricao) {
+        this._descricao = descricao
+    }; 
+
+    getId() {
+        return this.id
+    }
+
+    getNome() {
+        return this.nome
+    }
+
+    getDescricao() {
+        return this.descricao
+    }
+};
